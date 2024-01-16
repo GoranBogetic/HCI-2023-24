7122a107861045aba023d395328d94527362356b
Vedran Lasic <vedro.lasic@gmail.com>
2024-01-16 06:02:27 +0100

    Added search option in mugs page and added Contentful cms implementation on thermos page

--- semester-project/project/app/accessories/mugs/page.tsx
+++ semester-project/project/app/accessories/mugs/page.tsx
@@ -1,32 +1,53 @@
-"use client"
+'use client';
 
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 export default function Mugs() {
-	const [mugs, setMugs] = useState([]);
+  const [mugs, setMugs] = useState([]);
+  const [displaymugs, setDisplayMugs] = useState([]);
+  const [search, setSearch] = useState('');
 
-	useEffect(() => {
-		fetch("https://my-json-server.typicode.com/VLasic2001/mock-database-hci/mugs").then(response => response.json()).then(data => setMugs(data));
-	}, [])
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/VLasic2001/mock-database-hci/mugs')
+      .then((response) => response.json())
+      .then((data) => setMugs(data));
+  }, []);
 
-	return (
-		<h1>
-			<nav className="subNavbar">
-				<Link href="/accessories">Accessories</Link>
-				<Link href="/accessories/mugs">Coffee Mugs and Cups</Link>
-				<Link href="/accessories/thermoses">Travel Cups and Thermoses</Link>
-			</nav>
-			<span className="text">Coffee Mugs and Cups</span>
-			{mugs.map(mug => <div key={mug["id"]}>
-				<Link href={`./mugs/${mug["id"]}`}>
-					<h2>{mug["name"]}</h2>
-					<span>{mug["description"]}</span>
-				</Link>
-				<br />
-				<br />
-				</div>
-			)}
-		</h1>
-	);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setDisplayMugs(
+      mugs.filter((mug) => {
+        console.log(mug);
+        return mug.name.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  }, [mugs, search]);
+
+  return (
+    <h1>
+      <nav className="subNavbar">
+        <Link href="/accessories">Accessories</Link>
+        <Link href="/accessories/mugs">Coffee Mugs and Cups</Link>
+        <Link href="/accessories/thermoses">Travel Cups and Thermoses</Link>
+      </nav>
+      <span className="text">Coffee Mugs and Cups</span>
+      <div>
+        Search: <input type="text" placeholder="Enter search" className="searchInput" onChange={handleChange} />
+      </div>
+      {displaymugs.map((mug) => (
+        <div key={mug['id']}>
+          <Link href={`./mugs/${mug['id']}`}>
+            <h2>{mug['name']}</h2>
+            <span>{mug['description']}</span>
+          </Link>
+          <br />
+          <br />
+        </div>
+      ))}
+    </h1>
+  );
 }
--- semester-project/project/app/accessories/thermoses/page.tsx
+++ semester-project/project/app/accessories/thermoses/page.tsx
@@ -1,14 +1,89 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { it } from 'node:test';
 
-export default function Thermoses() {
-	return (
-		<h1>
-			<nav className="subNavbar">
-				<Link href="/accessories">Accessories</Link>
-				<Link href="/accessories/mugs">Coffee Mugs and Cups</Link>
-				<Link href="/accessories/thermoses">Travel Cups and Thermoses</Link>
-			</nav>
-			<span className="text">Travel Cups and Thermoses</span>
-		</h1>
-	);
+export interface TypeThermosListItem {
+  name: string;
+  description: string;
+  imageLink: string;
 }
+
+interface ThermosCollectionResponse {
+  thermosCollection: {
+    items: ThermosItem[];
+  };
+}
+
+interface ThermosItem {
+  name: string;
+  description: string;
+  imageLink: string;
+}
+
+const Thermoses = async () => {
+  const baseUrl = `https://graphql.contentful.com/content/v1/spaces/7ku0ysenhiwg/environments/master`;
+
+  const gqlGetThermosesQuery = `query ThermosList {
+		thermosCollection {
+			items {
+				name,
+				description,
+				imageLink
+			}
+		}
+	}`;
+
+  const getAllThermoses = async (): Promise<TypeThermosListItem[]> => {
+    try {
+      const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer Ok1oRhn9O3E0Ild34lXPsTw_4pWQo5SwBNq0O6qoNJc`,
+        },
+        body: JSON.stringify({ query: gqlGetThermosesQuery }),
+      });
+
+      const body = (await response.json()) as {
+        data: ThermosCollectionResponse;
+      };
+
+      console.log(response.json());
+
+      const thermoses: TypeThermosListItem[] = body.data.thermosCollection.items.map((item) => ({
+        name: item.name,
+        description: item.description,
+        imageLink: item.imageLink,
+      }));
+
+      return thermoses;
+    } catch (error) {
+      console.log(error);
+
+      return [];
+    }
+  };
+
+  const thermoses = await getAllThermoses();
+
+  return (
+    <h1>
+      <nav className="subNavbar">
+        <Link href="/accessories">Accessories</Link>
+        <Link href="/accessories/mugs">Coffee Mugs and Cups</Link>
+        <Link href="/accessories/thermoses">Travel Cups and Thermoses</Link>
+      </nav>
+      <span className="text">Travel Cups and Thermoses</span>
+      <div>
+        {thermoses.map((thermos) => (
+          <div key={thermos.name}>
+            <h3>{thermos.name}</h3>
+            <p>{thermos.description}</p>
+            <img src={thermos.imageLink} />
+          </div>
+        ))}
+      </div>
+    </h1>
+  );
+};
+
+export default Thermoses;
