7ccc123212cfffbb30aed6e90d229a168123f05d
Vedran Lasic <vedro.lasic@gmail.com>
2024-09-03 21:16:28 +0200

    Fix

--- semester-project/project/app/not-found.tsx
+++ semester-project/project/app/not-found.tsx
@@ -6,8 +6,8 @@ export default function Custom404() {
       <h1>Page Not Found</h1>
       <Image alt='Not found' src={"/404.jpg"} width={1000} height={0} />
       <p>
-        Uh, we can't seem to find the page you're looking for. Try going back to
-        the previous page
+        Uh, we can{"'"}t seem to find the page you{"'"}re looking for. Try going
+        back to the previous page
       </p>
     </div>
   );
